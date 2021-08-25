1423a6c6ced13439301e0545dd1b495311be52bb
devimalka <developerimalka@gmail.com>
2021-08-25 22:24:49 +0530

    added slideshow but autoslide conflicts

--- js/script.js
+++ js/script.js
@@ -1,15 +1,46 @@
-let slideIndex =0;
+//function for image sliding
+var slideIndex =1;
+showSlides(slideIndex);
 
-sliding();
-
-function sliding(){
-    let i;
-    let x = document.getElementsByClassName("slides");
-    for(i=0;i<x.length;i++){
-        x[i].style.display = "none";
-    }
-    slideIndex++;
-    if(slideIndex>x.length){slideIndex=1}
-    x[slideIndex-1].style.display = "block";
-    setTimeout(sliding,2000);
+function plusSlides(n){
+    showSlides(slideIndex += n);
 }
+
+function currentSlides(n){
+    showSlides(slideIndex=n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("myslide");
+    var dots = document.getElementsByClassName("dots");
+
+    if(n>slides.length){slideIndex=1}
+    if(n<1){slideIndex = slides.length}
+    for(i=0;i<slides.length;i++){
+        slides[i].style.display = "none";
+    }
+    for(i=0;i<dots.length;i++){
+        dots[i].className= dots[i].className.replace(" active","");
+
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active"
+}
+
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides(){
+//     var i;
+//     var slides = document.getElementsByClassName("myslide");
+//     for(i=0;i<slides.length;i++){
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if(slideIndex>slides.length){
+//         slideIndex=1
+//     }
+//     slides[slideIndex-1].style.display = "block";
+//     setTimeout(showSlides,5000);
+// }
